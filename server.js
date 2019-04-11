413ec9059d0c31f52a53851c99c96e1105682f1c
Jeremy Arrieta <jeremyretamararrieta@gmail.com>
2019-04-11 15:21:58 -0600

    corrected bugs

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const morgan = require('morgan')
 // const data = require('./product')
 const path = require("path")
+const PORT = process.env.PORT || 7000
 
 
 app.use(express.json())
@@ -21,7 +22,7 @@ app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 })
 
-app.listen(7000, () => {
+app.listen(PORT, () => {
     console.log('[+] Server is running on Port 7000')
 })
 
